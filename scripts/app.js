7b3eed5259fbe27fe6a208abf5eed40bd549e6c8
Jon Barson <jbarson@gmail.com>
2021-05-05 09:40:12 -0400

    looking at updating data

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
+
+function starDistance(starA, starB) {
+  //d = ((x2 - x1)2 + (y2 - y1)2 + (z2 - z1)2)1/2
+  return Math.pow(Math.pow(starA.Xg - starB.Xg, 2)+ Math.pow(starA.Yg - starB.Yg, 2) + Math.pow(starA.Zg - starB.Zg, 2),0.5)
+}
+
+
 /*(function () {
    "use strict";
    //the following is to link the slider with the text box*/
@@ -13,6 +20,22 @@
       }
     }
   });
+const systemsArr = []
+// const jumpList = []
+fetch('http://localhost:5500/NyrathStars.json')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(item => {
+      systemsArr.push(item)
+    })
+    data.forEach(item => {
+
+    })
+    //console.log(data)
+    // map.init();
+    // map.animate();
+  }).then(console.log)
+
 var map = map || {};
     map.systems = [];
     map.links = [];
@@ -65,93 +88,93 @@ var map = map || {};
     map.$epsi.change(function(){
       map.toggleEpsi();
     });
-map.init = function() {
-  map.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 75000 );
-  map.camera.position.z = 5000;
-  map.scene = new THREE.Scene();
-  for (var i in systemsArr){
-    var starText = "starText";
-    var system = systemsArr[i];
-    var starType = system.type[0][0].toUpperCase();
-    var systemDiv = document.createElement('div');
-    systemDiv.className = "starDiv";
-    var starPic = document.createElement('img');
-    if (starType==="A"){
-      starPic.src = 'img/A-star.png';
-    }else if (starType==="F"){
-      starPic.src = 'img/F-star.png';
-    }else if (starType==="G"){
-      starPic.src = 'img/G-star.png';
-    }else if (starType==="K"){
-      starPic.src = 'img/K-star.png';
-    }else if (starType==="M"){
-      starPic.src = 'img/M-star.png';
-    }else if (starType==="D"){
-      starPic.src = 'img/D-star.png';
-    }else{
-      starPic.src = 'img/spark1.png';
-    }
-    systemDiv.appendChild(starPic);
-    var name = document.createElement('div');
-    name.className = starText;
-    name.textContent = system.sysName;
-    systemDiv.appendChild(name);
-    if (system.planetName){var planet = document.createElement('div');
-      planet.className = starText;
-      planet.textContent = system.planetName;
-      systemDiv.appendChild(planet);
-    }
-    var star = new THREE.CSS3DObject(systemDiv);
-    star.position.x = system.x*map.Scale;
-    star.position.y = system.y*map.Scale;
-    star.position.z = system.z*map.Scale;
-    map.scene.add( star );
-    map.systems.push(star);
-  }
-  var systemIndex = _.pluck(systemsArr,"id");
-  for (var j in jumpList){
-    var startSys=systemsArr[_.indexOf(systemIndex,jumpList[j].bridge[0])];
-    var endSys=  systemsArr[_.indexOf(systemIndex,jumpList[j].bridge[1])];
-    var startPos=map.systems[_.indexOf(systemIndex,jumpList[j].bridge[0])].position;
-    var endPos=  map.systems[_.indexOf(systemIndex,jumpList[j].bridge[1])].position;
-    map.tmpVec1.subVectors( endPos, startPos );
-    var linkLength = map.tmpVec1.length() -25;
-    var hyperLink=document.createElement('div');
-    hyperLink.className="jumpLink";
-    if (jumpList[j].type==="A"){hyperLink.className="alpha";}
-    if (jumpList[j].type==="B"){hyperLink.className="beta";}
-    if (jumpList[j].type==="G"){hyperLink.className="gamma";}
-    if (jumpList[j].type==="D"){hyperLink.className="delta";}
-    if (jumpList[j].type==="E"){hyperLink.className="epsilon";}
-    hyperLink.style.height=  linkLength + "px";
-    var object = new THREE.CSS3DObject( hyperLink );
-    object.position.copy( startPos );
-    object.position.lerp( endPos, 0.5 );
-    var axis = map.tmpVec2.set( 0, 1, 0 ).cross( map.tmpVec1 );
-    var radians = Math.acos( map.tmpVec3.set( 0, 1, 0 ).dot( map.tmpVec4.copy( map.tmpVec1 ).normalize() ) );
-    var objMatrix = new THREE.Matrix4().makeRotationAxis( axis.normalize(), radians );
-    object.matrix = objMatrix;
-    object.rotation.setEulerFromRotationMatrix( object.matrix, object.eulerOrder );
-    object.matrixAutoUpdate = false;
-    object.updateMatrix();
-    if (object.element.className ==="alpha"){map.alphaLinks.push(object);}
-    if (object.element.className ==="beta"){map.betaLinks.push(object);}
-    if (object.element.className ==="delta"){map.deltaLinks.push(object);}
-    if (object.element.className ==="gamma"){map.gammaLinks.push(object);}
-    if (object.element.className ==="epsilon"){map.epsiLinks.push(object);}
-    map.scene.add( object );
-    map.links.push( object );
-  }
-  map.renderer = new THREE.CSS3DRenderer();
-  map.renderer.setSize( window.innerWidth, window.innerHeight );
-  document.getElementById( 'container' ).appendChild( map.renderer.domElement );
-  map.controls = new THREE.TrackballControls( map.camera, map.renderer.domElement );
-  map.controls.rotateSpeed = 0.05;
-  map.controls.dynamicDampingFactor = 0.3;
-  map.controls.maxDistance=7500;
-  map.controls.addEventListener( 'change', map.render );
-  window.addEventListener( 'resize', map.onWindowResize, false );
-};
+// map.init = function() {
+//   map.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 75000 );
+//   map.camera.position.z = 5000;
+//   map.scene = new THREE.Scene();
+//   for (var i in systemsArr){
+//     var starText = "starText";
+//     var system = systemsArr[i];
+//     var starType = system['Spectral Class'][0]?.toUpperCase();
+//     var systemDiv = document.createElement('div');
+//     systemDiv.className = "starDiv";
+//     var starPic = document.createElement('img');
+//     if (starType==="A"){
+//       starPic.src = 'img/A-star.png';
+//     }else if (starType==="F"){
+//       starPic.src = 'img/F-star.png';
+//     }else if (starType==="G"){
+//       starPic.src = 'img/G-star.png';
+//     }else if (starType==="K"){
+//       starPic.src = 'img/K-star.png';
+//     }else if (starType==="M"){
+//       starPic.src = 'img/M-star.png';
+//     }else if (starType==="D"){
+//       starPic.src = 'img/D-star.png';
+//     }else{
+//       starPic.src = 'img/spark1.png';
+//     }
+//     systemDiv.appendChild(starPic);
+//     var name = document.createElement('div');
+//     name.className = starText;
+//     name.textContent = system["Display Name"];
+//     systemDiv.appendChild(name);
+//     if (system.planetName){var planet = document.createElement('div');
+//       planet.className = starText;
+//       planet.textContent = system.planetName;
+//       systemDiv.appendChild(planet);
+//     }
+//     var star = new THREE.CSS3DObject(systemDiv);
+//     star.position.x = system.Xg*map.Scale;
+//     star.position.y = system.Yg*map.Scale;
+//     star.position.z = system.Zg*map.Scale;
+//     map.scene.add( star );
+//     map.systems.push(star);
+//   }
+//   var systemIndex = _.pluck(systemsArr,"HabHyg");
+//   for (var j in jumpList){
+//     // var startSys=systemsArr[_.indexOf(systemIndex,jumpList[j].bridge[0])];
+//     // var endSys=  systemsArr[_.indexOf(systemIndex,jumpList[j].bridge[1])];
+//     var startPos=map.systems[_.indexOf(systemIndex,jumpList[j].bridge[0])].position;
+//     var endPos=  map.systems[_.indexOf(systemIndex,jumpList[j].bridge[1])].position;
+//     map.tmpVec1.subVectors( endPos, startPos );
+//     var linkLength = map.tmpVec1.length() -25;
+//     var hyperLink=document.createElement('div');
+//     hyperLink.className="jumpLink";
+//     if (jumpList[j].type==="A"){hyperLink.className="alpha";}
+//     if (jumpList[j].type==="B"){hyperLink.className="beta";}
+//     if (jumpList[j].type==="G"){hyperLink.className="gamma";}
+//     if (jumpList[j].type==="D"){hyperLink.className="delta";}
+//     if (jumpList[j].type==="E"){hyperLink.className="epsilon";}
+//     hyperLink.style.height=  linkLength + "px";
+//     var object = new THREE.CSS3DObject( hyperLink );
+//     object.position.copy( startPos );
+//     object.position.lerp( endPos, 0.5 );
+//     var axis = map.tmpVec2.set( 0, 1, 0 ).cross( map.tmpVec1 );
+//     var radians = Math.acos( map.tmpVec3.set( 0, 1, 0 ).dot( map.tmpVec4.copy( map.tmpVec1 ).normalize() ) );
+//     var objMatrix = new THREE.Matrix4().makeRotationAxis( axis.normalize(), radians );
+//     object.matrix = objMatrix;
+//     object.rotation.setEulerFromRotationMatrix( object.matrix, object.eulerOrder );
+//     object.matrixAutoUpdate = false;
+//     object.updateMatrix();
+//     if (object.element.className ==="alpha"){map.alphaLinks.push(object);}
+//     if (object.element.className ==="beta"){map.betaLinks.push(object);}
+//     if (object.element.className ==="delta"){map.deltaLinks.push(object);}
+//     if (object.element.className ==="gamma"){map.gammaLinks.push(object);}
+//     if (object.element.className ==="epsilon"){map.epsiLinks.push(object);}
+//     map.scene.add( object );
+//     map.links.push( object );
+//   }
+//   map.renderer = new THREE.CSS3DRenderer();
+//   map.renderer.setSize( window.innerWidth, window.innerHeight );
+//   document.getElementById( 'container' ).appendChild( map.renderer.domElement );
+//   map.controls = new THREE.TrackballControls( map.camera, map.renderer.domElement );
+//   map.controls.rotateSpeed = 0.05;
+//   map.controls.dynamicDampingFactor = 0.3;
+//   map.controls.maxDistance=7500;
+//   map.controls.addEventListener( 'change', map.render );
+//   window.addEventListener( 'resize', map.onWindowResize, false );
+// };
 map.onWindowResize = function() {
   map.camera.aspect = window.innerWidth / window.innerHeight;
   map.camera.updateProjectionMatrix();
@@ -223,6 +246,8 @@ map.toggleEpsi = function(){
       }
 }};
 
-map.init();
-map.animate();
+
 /*}());*/
+
+
+const newJump = jumpList.map( item => {return{...item, distance: starDistance(systemsArr[item.bridge[0]], systemsArr[item.bridge[0]])}})
