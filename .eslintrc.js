e68abdc064a0814cd4165e9da62e14af59c6097e
Jon Barson <jbarson@gmail.com>
2025-04-06 10:53:57 -0400

    Refactor code structure for improved readability and maintainability

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
-    jquery: true
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   extends: 'eslint:recommended',
   rules: {
-    'no-extra-semi': 'error'
+    'no-extra-semi': 'error',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    'no-global-assign': 'warn'
+  },
+  globals: {
+    THREE: 'readonly',
+    _: 'readonly',
+    systemsArr: 'readonly',
+    jumpList: 'readonly'
   }
 };
